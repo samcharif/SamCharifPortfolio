633ae3bbfbf89aeca96ee576e24dbd74bb0778ba
Sam Charif <your-email@example.com>
2025-04-02 16:00:30 -0500

    Add dark mode toggle

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
     e.preventDefault();
     console.log("Form submitted!");
 });
+
+// Toggle dark mode
+document.getElementById('toggle-checkbox').addEventListener('change', function () {
+    document.body.classList.toggle('dark-mode');
+});
